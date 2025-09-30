f0d8eb856906810ee1906f97a3c68d58e8058be9
a.boikov <alex8oikov@yandex.ru>
2025-09-30 11:11:59 +0300

    презентер связывает модель со списком представлений, и каждое представление обновляет информацией о конкретном клиенте

---
+++ DemoLib/Presenters/ClientPresenter.cs
@@ -0,0 +1,25 @@
+﻿using DemoLib.Models;
+using DemoLib.Views;
+using System.Collections.Generic;
+
+namespace DemoLib.Presenters
+{
+    public class ClientPresenter
+    {
+        private readonly IClientsModel model_;
+        private readonly List<IClientView> views_ = new List<IClientView>();
+        public ClientPresenter(IClientsModel model, List<IClientView> views)
+        {
+            model_ = model;
+            views_ = views;
+
+            List<Client> allClients = model_.ReadAllClients();
+
+            for(int i = 0; i < allClients.Count; ++i)
+            {
+                Client client = allClients[i];
+                views[i].ShowClientInfo(client);
+            }
+        }
+    }
+}
