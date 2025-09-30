888141b0fa981eb408fbb217c2851a4d88bf1819
a.boikov <alex8oikov@yandex.ru>
2025-09-30 10:55:16 +0300

    добавлен проект библиотеки компонентов управления WinForms с пользовательским элементом для представления информации об одном клиенте

---
+++ ClientCard/ClientView.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ClientCard
+{
+    partial class ClientView
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.AvatarBox = new System.Windows.Forms.PictureBox();
+            this.DescriptionLabel = new System.Windows.Forms.Label();
+            this.PhoneLabel = new System.Windows.Forms.Label();
+            this.MailLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.AvatarBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TitleLabel.Location = new System.Drawing.Point(27, 23);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(92, 32);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "label1";
+            // 
+            // AvatarBox
+            // 
+            this.AvatarBox.Location = new System.Drawing.Point(223, 0);
+            this.AvatarBox.Name = "AvatarBox";
+            this.AvatarBox.Size = new System.Drawing.Size(232, 200);
+            this.AvatarBox.TabIndex = 1;
+            this.AvatarBox.TabStop = false;
+            // 
+            // DescriptionLabel
+            // 
+            this.DescriptionLabel.AutoSize = true;
+            this.DescriptionLabel.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DescriptionLabel.Location = new System.Drawing.Point(56, 122);
+            this.DescriptionLabel.Name = "DescriptionLabel";
+            this.DescriptionLabel.Size = new System.Drawing.Size(63, 25);
+            this.DescriptionLabel.TabIndex = 2;
+            this.DescriptionLabel.Text = "label1";
+            // 
+            // PhoneLabel
+            // 
+            this.PhoneLabel.AutoSize = true;
+            this.PhoneLabel.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PhoneLabel.Location = new System.Drawing.Point(220, 220);
+            this.PhoneLabel.Name = "PhoneLabel";
+            this.PhoneLabel.Size = new System.Drawing.Size(38, 14);
+            this.PhoneLabel.TabIndex = 3;
+            this.PhoneLabel.Text = "label1";
+            // 
+            // MailLabel
+            // 
+            this.MailLabel.AutoSize = true;
+            this.MailLabel.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MailLabel.Location = new System.Drawing.Point(357, 220);
+            this.MailLabel.Name = "MailLabel";
+            this.MailLabel.Size = new System.Drawing.Size(38, 14);
+            this.MailLabel.TabIndex = 4;
+            this.MailLabel.Text = "label1";
+            // 
+            // ClientView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.Controls.Add(this.MailLabel);
+            this.Controls.Add(this.PhoneLabel);
+            this.Controls.Add(this.DescriptionLabel);
+            this.Controls.Add(this.AvatarBox);
+            this.Controls.Add(this.TitleLabel);
+            this.Name = "ClientView";
+            this.Size = new System.Drawing.Size(455, 246);
+            ((System.ComponentModel.ISupportInitialize)(this.AvatarBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.PictureBox AvatarBox;
+        private System.Windows.Forms.Label DescriptionLabel;
+        private System.Windows.Forms.Label PhoneLabel;
+        private System.Windows.Forms.Label MailLabel;
+    }
+}
