78ca0069434b90942e1aceb04a9fbc0e8242fb0b
a.boikov <alex8oikov@yandex.ru>
2025-09-30 10:17:48 +0300

    ввели описание сущности клиента в приложение

---
+++ DemoLib/Client.cs
@@ -0,0 +1,20 @@
+﻿namespace DemoLib
+{
+    public class Client
+    {
+        private int id_;
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Mail { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public Client(int id)
+        {
+            id_ = id;
+        }
+    }
+}
