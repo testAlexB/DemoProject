8afd62917dfab36c5df3e592fa50f839a987e0fb
a.boikov <alex8oikov@yandex.ru>
2025-09-30 11:02:36 +0300

    перенесли шаблон карточки на главную форму добавили иконку и заголовок формы главного окна

---
+++ DemoWinForms/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace DemoProject
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.clientView1 = new ClientCard.ClientView();
+            this.SuspendLayout();
+            // 
+            // clientView1
+            // 
+            this.clientView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.clientView1.Location = new System.Drawing.Point(12, 12);
+            this.clientView1.Name = "clientView1";
+            this.clientView1.Size = new System.Drawing.Size(455, 246);
+            this.clientView1.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clientView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Управление клиентами ОАО \"Цемент-Строй\"";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private ClientCard.ClientView clientView1;
+    }
+}
+
