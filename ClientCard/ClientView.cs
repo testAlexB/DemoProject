9b44d6d3eef9c3a02ea2dc393b4774116dcf2a38
a.boikov <alex8oikov@yandex.ru>
2025-09-30 11:14:14 +0300

    реализовали отображение информации о клиенте в его карточке

--- ClientCard/ClientView.cs
+++ ClientCard/ClientView.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DemoLib.Views;
+using DemoLib;
 
 namespace ClientCard
 {
-    public partial class ClientView: UserControl
+    public partial class ClientView: UserControl, IClientView
     {
         public ClientView()
         {
             InitializeComponent();
+        }
+
+        public void ShowClientInfo(Client client)
+        {
+            TitleLabel.Text = client.Name;
+            DescriptionLabel.Text = client.Description;
+            PhoneLabel.Text = client.Phone;
+            MailLabel.Text = client.Mail;
+            AvatarBox.Load(client.ImagePath);
         }
     }
 }
