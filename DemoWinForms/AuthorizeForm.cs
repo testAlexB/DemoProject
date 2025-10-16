4f7649d83e38727c85a5a4d0471460ad60b4f3dd
a.boikov <alex8oikov@yandex.ru>
2025-10-16 09:07:30 +0300

    разработали макет формы авторизации сделали запуск этой формы при старте программы

---
+++ DemoWinForms/AuthorizeForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoProject
+{
+    public partial class AuthorizeForm : Form
+    {
+        public AuthorizeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AuthorizeForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PasswordTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- DemoWinForms/Program.cs
+++ DemoWinForms/Program.cs
@@ -16,7 +16,7 @@ namespace DemoProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new AuthorizeForm());
         }
     }
 }
