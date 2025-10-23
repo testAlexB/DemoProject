e1cffff3f559af58d1eba115803accdd384cd9ce
a.boikov <alex8oikov@yandex.ru>
2025-10-23 08:40:22 +0300

    реализовали метод получения информации обо всех клиентах на основе данных из БД MySQL

--- DemoLib/Models/Clients/MySQLClientsModel.cs
+++ DemoLib/Models/Clients/MySQLClientsModel.cs
@@ -1,26 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using MySql.Data.MySqlClient;
 
 namespace DemoLib.Models.Clients
 {
     public class MySQLClientsModel : IClientsModel
     {
+        private const string connStr = "server=localhost;user=root;database=clients_db;password=1234567;port=3307;";
         public int GetClientsCount()
         {
-            string connStr = "server=localhost;user=root;database=clients_db;password=1234567;port=3307;";
-
             try
             {
-                MySqlConnection connection = new MySqlConnection(connStr);
-                connection.Open();
+                using (MySqlConnection connection = new MySqlConnection(connStr))
+                {
+                    connection.Open();
 
-                string sql = "SELECT COUNT(id) FROM clientsinfo";
-                MySqlCommand command = new MySqlCommand(sql, connection);
-                int result = Convert.ToInt32(command.ExecuteScalar().ToString());
-                connection.Close();
+                    string sql = "SELECT COUNT(id) FROM clientsinfo";
+                    MySqlCommand command = new MySqlCommand(sql, connection);
+                    int result = Convert.ToInt32(command.ExecuteScalar().ToString());
 
-                return result;
+                    return result;
+                }
             }
             catch (Exception ex)
             {
@@ -32,30 +33,38 @@ namespace DemoLib.Models.Clients
         {
             List<Client> clients = new List<Client>();
 
-            string connStr = "server=localhost;user=root;database=clients_db;password=1234567;port=3307;";
-
             try
             {
-                MySqlConnection connection = new MySqlConnection(connStr);
-                connection.Open();
+                using (MySqlConnection connection = new MySqlConnection(connStr))
+                {
+                    connection.Open();
 
-                string sql = "SELECT id, clientName, phone, mail, description, imagePath FROM clientsinfo";
-                MySqlCommand command = new MySqlCommand(sql, connection);
+                    string sql = "SELECT id, clientName, phone, mail, description, imagePath FROM clientsinfo";
+                    MySqlCommand command = new MySqlCommand(sql, connection);
 
-                command.ExecuteReader();
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Client client = new Client(reader.GetInt32(0));
 
+                            client.Name = reader.GetString(1);
+                            client.Phone = reader.GetString(2);
+                            client.Mail = reader.GetString(3);
+                            client.Description = reader.GetString(4);
+                            client.ImagePath = reader.GetString(5);
 
+                            clients.Add(client);
+                        }
+                    }
 
-                connection.Close();
-
-                return result;
+                    return clients;
+                }
             }
             catch (Exception ex)
             {
                 throw ex;
             }
-
-            return clients;
         }
     }
 }
